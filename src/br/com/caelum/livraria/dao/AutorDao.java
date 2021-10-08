fe39247702f6e0d4f59f00bbc27cae3792e68ef0
fernando <fernando@localhost>
2021-10-07 22:44:23 -0300

    Aula 3 - Integração do JPA com Pool e Datasource

--- src/br/com/caelum/livraria/dao/AutorDao.java
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -4,15 +4,16 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
-import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
 public class AutorDao {
 
-	@Inject
-	private Banco banco;
+	@PersistenceContext
+	private EntityManager manager;
 	
 	@PostConstruct
 	void aposCriacao() {
@@ -23,17 +24,17 @@ public class AutorDao {
 		
 		System.out.println("Salvando o autor" + autor.getNome());
 		
-		banco.save(autor);
+		manager.persist(autor);
 		
 		System.out.println("Salvo " + autor.getNome());
 	}
 	
 	public List<Autor> todosAutores() {
-		return banco.listaAutores();
+		return manager.createQuery("select a from Autor a", Autor.class).getResultList();
 	}
 
 	public Autor buscaPelaId(Integer autorId) {
-		Autor autor = this.banco.buscaPelaId(autorId);
+		Autor autor = this.manager.find(Autor.class, autorId);
 		return autor;
 	}
 	
