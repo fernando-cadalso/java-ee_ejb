89a109b8f34ffc6370e6a03d4c564f4ab4caadd3
Fernando de Alcântara Cadalso <fernando.cadalsoii@gmail.com>
2021-10-11 20:07:01 -0300

    Aulas 5 e 6 Lidando com exceções e interceptadores

--- src/br/com/caelum/livraria/dao/AutorDao.java
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -4,33 +4,36 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
+import javax.interceptor.Interceptors;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import br.com.caelum.livraria.interceptador.LogInterceptador;
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
+//@Interceptors({LogInterceptador.class})
 public class AutorDao {
 
 	@PersistenceContext
 	private EntityManager manager;
-	
+
 	@PostConstruct
 	void aposCriacao() {
 		System.out.println("[INFO Construcao] AutorDao carregado.");
 	}
 
 	public void salva(Autor autor) {
-		
+
 		System.out.println("Salvando o autor" + autor.getNome());
-		
+
 		manager.persist(autor);
-		
+
 		System.out.println("Salvo " + autor.getNome());
+
+//		throw new LivrariaException();
 	}
-	
+
 	public List<Autor> todosAutores() {
 		return manager.createQuery("select a from Autor a", Autor.class).getResultList();
 	}
@@ -39,5 +42,5 @@ public class AutorDao {
 		Autor autor = this.manager.find(Autor.class, autorId);
 		return autor;
 	}
-	
+
 }
